86b2ba45814142c1e7caaf7383c3d25f07139df8
raul coan <raul@SERVIDORT300>
2018-09-04 16:14:25 -0300

    translation

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Ops, esse usúario não foi encontrado.',
+    'throttle' => 'Muitas tentativas de conexão neste usuario, aguarde :seconds segundos para tentar novamente.',
 
 ];
